7fdd5640a842420d68338ab4d3a97b729c135f6b
luchoman08 <luis.manrique@correounivalle.edu.co>
2016-01-28 20:01:16 +0100

    update_main

--- main.cpp
+++ main.cpp
@@ -11,17 +11,27 @@ class hoja{
  bool hoja_final;
  
  public:
+ hoja();
+
  hoja* get_padre();
  hoja* get_hijo_por_posicion(int posicion);
  hoja* get_hijo_por_caracter(char posicion);
- 
  vector <hoja*> get_hijos();
+ void set_caracter(char nuevo_caracter);
     
 };
+hoja* hoja::get_padre()
+{
+return this->padre;
+}
+void hoja::set_caracter(char nuevo_caracter)
+{
+	
+}
 
 int main()
 {
-	
+  hoja padre,hijo;
   std::string name;
   std::cout << "What is your name? ";
   getline (std::cin, name);
