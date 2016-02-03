32b837b027ed3bf81188e86f4609472e9f5f5283
luchoman08 <luchoman08@gmail.com>
2016-02-03 15:34:38 -0500

    nuevo programa

---
+++ Hoja.hpp
@@ -0,0 +1,29 @@
+#ifndef Hoja_hpp
+#define Hoja_hpp
+#include <string>
+#include <vector>
+
+class Hoja{
+ private:
+ char caracter;
+ std::vector<Hoja*> hijos;
+ Hoja* padre;
+ bool hoja_final;
+ 
+ public:
+ 
+ Hoja();
+ ~Hoja();
+
+ Hoja* get_padre();
+ Hoja* get_hijo_por_posicion(int posicion);
+ Hoja* get_hijo_por_caracter(char caracter);
+ std::vector <Hoja*> get_hijos();
+ void set_caracter(char nuevo_caracter);
+ char get_caracter();
+ void adicionar_hijo(Hoja *hijo);
+ void set_padre(Hoja *padre);
+ bool existencia_hijo_por_caracter(char caracter);
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,39 +1,82 @@
-// Example program
 #include <iostream>
-#include <string>
-#include <vector>
+#include "Hoja.hpp"
 using namespace std;
-class hoja{
- private:
- char caracter;
- vector<hoja*> hijos;
- hoja* padre;
- bool hoja_final;
- 
- public:
- hoja();
 
- hoja* get_padre();
- hoja* get_hijo_por_posicion(int posicion);
- hoja* get_hijo_por_caracter(char posicion);
- vector <hoja*> get_hijos();
- void set_caracter(char nuevo_caracter);
-    
-};
-hoja* hoja::get_padre()
-{
+Hoja::Hoja(){
+}
+Hoja::~Hoja(){
+}
+Hoja* Hoja::get_padre(){
 return this->padre;
 }
-void hoja::set_caracter(char nuevo_caracter)
-{
-	
+
+
+void Hoja::set_caracter(char nuevo_caracter){
+	this->caracter=nuevo_caracter;
 }
 
-int main()
+char Hoja::get_caracter(){
+	
+	return this->caracter;
+}
+void Hoja::adicionar_hijo(Hoja *hijo){
+	if(!existencia_hijo_por_caracter(hijo->get_caracter()))
+	this->hijos.push_back(hijo);
+	}
+	
+Hoja* Hoja::get_hijo_por_posicion(int pos){
+return hijos.at(pos);	
+}
+
+void Hoja::set_padre(Hoja *padre){
+	this->padre = padre;
+}
+Hoja* Hoja::get_hijo_por_caracter(char caracter)
 {
-  hoja padre,hijo;
-  std::string name;
-  std::cout << "What is your name? ";
-  getline (std::cin, name);
-  std::cout << "Hello word, " << name << "!\n";
+	int numero_hijos = this->hijos.size();
+	for(int i = 0; i< numero_hijos; i++)
+	{
+		if(hijos.at(i)->caracter==caracter)
+		return hijos.at(i);	
+	}
+	return hijos.at(0);
+	
+}
+bool Hoja::existencia_hijo_por_caracter(char caracter)
+{
+	int numero_hijos = this->hijos.size();
+	for(int i = 0; i< numero_hijos; i++)
+	{
+		if(hijos.at(i)->caracter==caracter)
+		return true;		
+	}
+	return false;
+}
+vector <Hoja*> Hoja::get_hijos()
+{
+	return this->hijos;
+}
+
+void imprimir_hijos_arbol(Hoja* arbol)
+{
+	int size = arbol->get_hijos().size();
+	for(int i = 0; i < size; i++)
+	{
+		cout<<arbol->get_hijos().at(i)->get_caracter()<<" ";
+	}
+}
+
+void inicializar_padre(Hoja *padre)
+{
+  padre->set_caracter('_');
+  padre->set_padre(padre);
+
+}
+int main(int argv, char** args){
+  Hoja padre;
+  inicializar_padre(&padre);
+  //imprimir_hijos_arbol(&padre);
+  cout<<padre.existencia_hijo_por_caracter('o')<<endl;
+  //cout<<padre.get_hijo_por_posicion(0)->get_caracter()<<padre.get_hijo_por_posicion(1)->get_caracter()<<endl;
+  return 0;
 }
